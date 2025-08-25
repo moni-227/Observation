482ad838218ae784403a4db5feeaef88c861a2c7
moni-227 <146161468+moni-227@users.noreply.github.com>
2025-08-25 16:26:25 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://observationm.onrender.com/api", // backend url
+    baseURL: "https://observe-api.onrender.com/api", // ✅ Your BACKEND URL
 });
 
 export const getObservations = () => API.get("/observations");
 export const getObservation = (id) => API.get(`/observations/${id}`);
 export const createObservation = (data) => API.post("/observations", data);
+
  
