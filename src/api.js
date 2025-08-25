9331d1c422aad1e827f53b3eb5b7feb2c2f34938
moni-227 <146161468+moni-227@users.noreply.github.com>
2025-08-25 15:56:37 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: "https://observationm.onrender.com/api", // backend url
+});
+
+export const getObservations = () => API.get("/observations");
+export const getObservation = (id) => API.get(`/observations/${id}`);
+export const createObservation = (data) => API.post("/observations", data);
+ 
