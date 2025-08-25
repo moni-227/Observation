f9c32e0d36297c8085c41430d1c470eec4290e61
moni-227 <146161468+moni-227@users.noreply.github.com>
2025-08-25 16:10:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config();require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -6,24 +6,29 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
+
+// Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-app.use(cors());
+app.use(cors({
+  origin: ["https://observationm.onrender.com", "http://localhost:3000"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(express.json());    // parse JSON bodies
 
 app.get('/', (_req, res) => res.send('Observation API is running'));
 
+// Routes
 app.use('/api/observations', require('./routes/observations'));
 
-const PORT = 'mongodb://localhost:27017/safety' || 5000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
+// DB Connection
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://admin:admin@crm.emoz4sc.mongodb.net/Observation')
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`API listening on :${PORT}`));
+    app.listen(process.env.PORT || 5000, () => console.log(`API running`));
   })
   .catch(err => console.error('Mongo connect error:', err));
+
+
