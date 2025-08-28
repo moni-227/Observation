faaebc59642a933d4a9e1791917b2fd8ee759bab
moni-227 <146161468+moni-227@users.noreply.github.com>
2025-08-28 11:24:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // ✅ Add this
 
 
 function App() {
