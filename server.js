2adf769bad06385024ee9f8c5fc829e40ae77a0c
moni-227 <146161468+moni-227@users.noreply.github.com>
2025-08-28 10:29:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 
 const app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Middleware
@@ -39,3 +39,4 @@ mongoose
     app.listen(PORT, () => console.log(`✅ API running on port ${PORT}`));
   })
   .catch(err => console.error('❌ MongoDB connection error:', err));
+
